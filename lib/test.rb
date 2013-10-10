ad8406beed909cd5985c8f238ecc51039b69f227
Aaron Lifton <theflubba@gmail.com>
2013-10-10 13:37:49 -0700

    yes

--- lib/test.rb
+++ lib/test.rb
@@ -6,6 +6,8 @@ p ø(String) == ""
 p ø(Fixnum) == 0
 x = λ {|y| y + 1 }
 p x[2]
+y = λ {|x, y| x + y}
+p y[1,2]
 
 p ∀ [1,2,"cat",3] { |e| e.is_a?(Fixnum) }
 p ∃ [1,2,"cat",3] { |e| e.is_a?(Fixnum) }
